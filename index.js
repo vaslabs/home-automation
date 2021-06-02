df2aa6d4cf4f812eea47af271e274a31166bb53e
Vasilis Nicolaou <vaslabsco@gmail.com>
2021-06-02 12:46:20 +0100

    Minimal setup to serve management commands

--- index.js
+++ index.js
@@ -3,30 +3,24 @@ const { exec } = require("child_process");
 
 const PC_MAC_ADDRESS = process.env.PC_MAC_ADDRESS
 const wakeOnLanCommand = PC_MAC_ADDRESS ? `wakeonlan ${PC_MAC_ADDRESS}` : "echo NoOp"
-
+const QUEUE_URL = process.env.QUEUE_URL
+const RESTART_STEAM_COMMAND = PC_MAC_ADDRESS ? "echo NoOp" : "/usr/bin/kill --verbose --timeout 3000 TERM --timeout 7000 KILL --signal QUIT $(pgrep steam | head -n1) && sleep 5 && DRI_PRIME=1 steam &"
 const mapping = {
-  "StartOvercooked": {message: "Starting overcooked", id: "1243830"},
-  "StartCantDriveThis": {message: "Starting can't drive this", id: "466980"},
-  "StartFootball": {message: "Starting PES2021", id: "1259970"},
-  "StartCyberpunk": {message: "Starting cyberpunk", id: "1091500"},
-  "StartPlagueSimulation": {message: "Starting plague simulation", id: "246620"},
-  "StartPlagueTaleInnocence": {message: "Starting plague tale: innocence", id: "752590"},
-  "StartUltimateChickenHorse": {message: "Starting ultimate chicket horse", id: "386940"},
   "TurnOnPc": {command: wakeOnLanCommand},
-  "TurnOffPc": {command: "poweroff"}
+  "TurnOffPc": {command: "poweroff"},
+  "RestartSteam": {command: RESTART_STEAM_COMMAND},
+  "RestartPc": {command: "reboot"}
 }
 
 const app = Consumer.create({
-  queueUrl: 'https://sqs.eu-west-1.amazonaws.com/459553275750/home_actions',
+  queueUrl: QUEUE_URL,
   handleMessage: async (message) => {
     const intent = JSON.parse(message.Body)
     console.log(intent)
     const game = mapping[intent.name]
     if (game) {
       console.log(game.message)
-      if (game.id) {
-        startGame(game.id)
-      } else if (game.command) {
+      if (game.command) {
         run(game.command)
       }
     }
@@ -42,22 +36,6 @@ app.on('processing_error', (err) => {
 });
  
 app.start();
-
-function startGame(gameId) {
-
-    exec(`steam steam://rungameid/${gameId} &`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
-}
-
 
 function run(command) {
   exec(`${command} &`, (error, stdout, stderr) => {
